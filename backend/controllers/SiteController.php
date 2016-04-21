d1589d799593295c1ba0d23bc6cf1d572b14a83e
jeredfloyd <jered@convivian.com>
2016-04-21 16:21:54 -0400

    add missing exception imports

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -4,6 +4,7 @@ namespace backend\controllers;
 
 use Yii;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use common\models\LoginForm;
 use common\models\ContactForm;
